639de90eeb77bb40e960c242984630342fda7720
zhouxl <zhouxl@ckt.cn>
2026-04-03 13:38:59 +0800

    feat(ast): add KernelExpr enum for expression AST

--- crates/sile-macros/src/frontend/ast.rs
+++ crates/sile-macros/src/frontend/ast.rs
@@ -12,14 +12,46 @@ pub struct KernelParam {
 }
 
 #[derive(Clone, Debug)]
-#[allow(dead_code)]
 pub enum KernelStmt {
     Let {
         name: syn::Ident,
-        expr: syn::Expr,
+        expr: KernelExpr,
     },
     Store {
         target: syn::Ident,
-        value: syn::Expr,
+        value: KernelExpr,
     },
+}
+
+#[derive(Clone, Debug)]
+pub enum KernelExpr {
+    Var(syn::Ident),
+    Lit(syn::LitInt),
+    MethodCall {
+        receiver: Box<KernelExpr>,
+        method: syn::Ident,
+        args: Vec<KernelExpr>,
+    },
+    Call {
+        func: syn::Ident,
+        args: Vec<KernelExpr>,
+    },
+    BinaryOp {
+        left: Box<KernelExpr>,
+        op: BinOpKind,
+        right: Box<KernelExpr>,
+    },
+    Array(syn::ExprArray),
+    FieldAccess {
+        target: Box<KernelExpr>,
+        field: syn::Ident,
+    },
+}
+
+#[derive(Clone, Copy, Debug)]
+pub enum BinOpKind {
+    Add,
+    Sub,
+    Mul,
+    Div,
 }
